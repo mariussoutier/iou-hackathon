8edd74cc2abc7b5fb99581db2f27908eef0f82bb
Marius Soutier <spybreak@gmx.net>
2012-06-22 17:46:54 +0200

    Better URL for summaries

--- app/models/User.scala
+++ app/models/User.scala
@@ -3,7 +3,6 @@ package models
 import play.api.Play.current
 
 import com.novus.salat.dao._
-import com.novus.salat.annotations._
 import com.mongodb.casbah.Imports._
 import se.radley.plugin.salat._
 
